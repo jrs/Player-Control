6afe550b98cffcb99c27abcd342c9471d13eb79d
Justin Scott <hello@jrs.dev>
2022-08-12 20:00:50 -0700

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
     }
 }
